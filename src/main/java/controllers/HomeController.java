0b895a5e6ecbdceba3834f70dc03edd744e8ed0b
AN VAN <vanxuanbinhan01@gmail.com>
2018-06-10 00:04:08 +0700

    add source

---
+++ src/main/java/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by nhs3108 on 6/2/17.
+ */
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        System.out.println("demodemo");
+        return "index";
+    }
+}
